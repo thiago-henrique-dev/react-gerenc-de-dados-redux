37fa64430ae4240a2b265276d7544a64a51cb98f
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-12 13:49:54 -0300

    Componente Intervalo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux(Simples)</h1>
       <div className="linha">
-        <Card title="Card 1" red>Good morning</Card>
-        <Card title="Card 2" green>Hellow World</Card>
+        <Intervalo red></Intervalo>
 
       </div>
       <div className="linha">
+        <Card title="Card 2" green>Hellow World</Card>
         <Card title="Card 3" blue>Welcome!</Card>
         <Card title="Card 4" purple>Hellow World</Card>
 
---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,24 @@
+import './Intervalo.css'
+import React from 'react'
+import Card from './Card'
+
+const Intervalo = props => {
+    return (
+        <Card title="Intervalo de numeros" red>
+            <div className="Intervalo">
+                <span>
+                  <strong>Mínimo:</strong>
+                     <input type="number" value={0} readOnly/>
+                 </span>
+                 <span>
+                    <strong>Máximo:</strong>
+                     <input type="number" value={10} readOnly/>
+                 </span>
+            </div>
+
+            
+        </Card>
+    )
+}
+
+export default Intervalo
