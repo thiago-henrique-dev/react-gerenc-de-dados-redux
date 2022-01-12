a9ddb3110142a06ff8870176f6242bb726098867
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-12 13:32:11 -0300

    Componente Card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
@@ -7,14 +6,14 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux(Simples)</h1>
       <div className="linha">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 1" red>Good morning</Card>
+        <Card title="Card 2" green>Hellow World</Card>
+
       </div>
       <div className="linha">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 3" blue>Welcome!</Card>
+        <Card title="Card 4" purple>Hellow World</Card>
+
       </div>
     </div>  
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,29 @@
 import './Card.css'
 import React from 'react'
 
-const teste = props => {
+function getColor(props){
+    if(props.red) return "Red"
+    if(props.green) return "Green"
+    if(props.blue) return "Blue"
+    if(props.purple) return "Purple"
+    return ""
+
+
+
+}
+
+
+const card = props => {
     return(
-        <div>
-            Card
+        <div className={`Card ${getColor(props)}`}>
+            <div className="Header">
+                <span className="Title">{props.title}</span>
+            </div>
+            <div className="Content">
+                {props.children}
+            </div>
         </div>
     )
 }
 
-export default teste
+export default card
